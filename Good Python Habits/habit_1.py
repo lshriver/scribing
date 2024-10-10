6cdb76982212cdfec78042df973f52711d3ff30e
Lauren Shriver <lshriver@protonmail.com>
2024-10-10 23:13:47 +0000

    5 Good Python Habits folder ✅

--- Good Python Habits/habit_1.py
+++
@@ -1,4 +0,0 @@
-from habit_1_module import connect
-
-if __name__ == '__main__':
-  connect()
--- Good Python Habits/habit_4.py
+++
@@ -1,5 +0,0 @@
- # Habit 4 - Type Annotations
-def upper_everything(elements: list[str]) -> list[str]:
-  return [element.upper() for element in elements]
-
-loud_list = list[str] = upper_everything(['renzo', 'jesse', 'paul', 'allison'])
